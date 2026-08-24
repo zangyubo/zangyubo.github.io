9a06c6675281957329dd06b0e9cea31f7a823c18
ZangYubo <129265134+zangyubo@users.noreply.github.com>
2026-08-24 17:01:31 +0800

    添加获取个人网站的文档和样式，更新主题布局

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,4 +1,41 @@
 // Learn how to customize the theme: https://rspress.rs/guide/basic/custom-theme
 import './index.css';
+import {
+  LastUpdated,
+  Layout as BasicLayout,
+  Banner,
+  PrevNextPage,
+} from '@rspress/core/theme-original';
+import { useLang } from '@rspress/core/runtime';
 
 export * from '@rspress/core/theme-original';
+
+export function DocFooter() {
+  return (
+    <footer className="rp-doc-footer">
+      <div className="rp-doc-footer__edit">
+        <div className="site-author">by -- navrow</div>
+        <LastUpdated />
+      </div>
+      <div className="rp-doc-footer__divider" />
+      <PrevNextPage />
+    </footer>
+  );
+}
+
+export function Layout() {
+  const lang = useLang();
+
+  return (
+    <BasicLayout
+      beforeNav={
+        <Banner
+          href="/guide/start/introduction.html"
+          message={
+            lang === 'en' ? 'Website is under development' : '网站开发中'
+          }
+        />
+      }
+    />
+  );
+}
